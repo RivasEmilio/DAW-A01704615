2c086e4d468d03b4644bc7b46891008c770a2fae
RivasEmilio <miles.fra.2000@gmail.com>
2020-10-05 22:48:47 -0500

    hotfix

--- lab14-15/index.php
+++ lab14-15/index.php
@@ -39,12 +39,13 @@
 
     if(isset($_POST["submit-fruit"]) && !empty($_POST["submit-fruit"])){
 
+        $_POST["namefruit"]=htmlspecialchars($_POST["namefruit"]);
         $name = $_POST["namefruit"];
         $quantity = $_POST["quantityfruit"];
         $price = $_POST["pricefruit"];
 
-        if(isset($_POST["namefruit"]) && !empty($_POST["namefruit"]) && isset($_POST["quantityfruit"]) && !empty($_POST["quantityfruit"]) && isset($_POST["pricefruit"]) && !empty($_POST["pricefruit"])){
-            
+        if(isset($_POST["namefruit"]) && !empty($_POST["namefruit"]) && isset($_POST["quantityfruit"]) && !empty($_POST["quantityfruit"]) && isset($_POST["pricefruit"]) && !empty($_POST["pricefruit"]) && ctype_alpha($name)){
+           
             insertFruit($name, $quantity, $price);
             header('Location: '.$_SERVER['REQUEST_URI']);
         }else{
@@ -55,9 +56,11 @@
 
     if(isset($_POST["delete-fruit"]) && !empty($_POST["delete-fruit"])){
 
+        $_POST["nfd"]=htmlspecialchars($_POST["nfd"]);
         $name = $_POST["nfd"];
 
-        if(isset($_POST["nfd"]) && !empty($_POST["nfd"]) ){
+        if(isset($_POST["nfd"]) && !empty($_POST["nfd"] && ctype_alpha($name)) ){
+     
             echo "eliminado de base de datos";
             deleteFruit($name);
             header('Location: '.$_SERVER['REQUEST_URI']);
@@ -70,12 +73,15 @@
 
     if(isset($_POST["mod-fruit"]) && !empty($_POST["mod-fruit"])){
 
+        $_POST["nfm"]=htmlspecialchars($_POST["nfm"]);
         $name = $_POST["nfm"];
         $quantity = $_POST["qfm"];
         $price = $_POST["pfm"];
+       
 
-        if(isset($_POST["nfm"]) && !empty($_POST["nfm"]) && isset($_POST["qfm"]) && !empty($_POST["qfm"]) && isset($_POST["pfm"]) && !empty($_POST["pfm"])){
-
+        if(isset($_POST["nfm"]) && !empty($_POST["nfm"]) && isset($_POST["qfm"]) && !empty($_POST["qfm"]) && isset($_POST["pfm"]) && !empty($_POST["pfm"]) && ctype_alpha($name)){
+            
+            
             updateDB($name,$quantity,$price);
             header('Location: '.$_SERVER['REQUEST_URI']);
             
